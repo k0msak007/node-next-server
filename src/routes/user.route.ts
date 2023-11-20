5a4b5d2e1a8821d82ae377029bad511866cf9025
komsak007 <98792390+k0msak007@users.noreply.github.com>
2023-11-20 13:09:34 +0700

    Initial Commit

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,9 @@
 import express from "express"
 import { registerValidator } from "../utils/validators/userValidator"
 import { validatorResult } from "../middlewares/validator/validatorResult.middleware"
-import moduleName from 'assert';
 
 const router = express.Router()
 
-router.post("/register", registerValidator, validatorResult)
+router.post("/register", registerValidator, validatorResult);
 
 export default router
