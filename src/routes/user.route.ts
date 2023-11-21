db454bdd94b33463811e852d46842bee384ca846
komsak007 <98792390+k0msak007@users.noreply.github.com>
2023-11-21 09:40:18 +0700

    Connected database with mssql

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import { registerValidator } from "../utils/validators/userValidator"
 import { validatorResult } from "../middlewares/validator/validatorResult.middleware"
+import { Register } from "../controllers/example.controller"
 
 const router = express.Router()
 
-router.post("/register", registerValidator, validatorResult)
+router.post("/register", registerValidator, validatorResult,)
+router.get("/register", Register)
 
 export default router
